f81e827bc74c9f24b965c6872a51d54163fb2751
milicamisic <milicamisic2303@gmail.com>
2025-10-05 23:26:40 +0200

    [add] bedsheet pravila rade

--- sbnz-integracija-projekta/service/src/main/java/com/ftn/sbnz/service/config/DroolsConfig.java
+++ sbnz-integracija-projekta/service/src/main/java/com/ftn/sbnz/service/config/DroolsConfig.java
@@ -29,6 +29,7 @@ public class DroolsConfig {
         kfs.write(ResourceFactory.newClassPathResource("rules/forward/shirt/shirt-fit-correction.drl"));
         kfs.write(ResourceFactory.newClassPathResource("rules/forward/curtain-rules.drl"));
         kfs.write(ResourceFactory.newClassPathResource("rules/forward/tablecloth-rules.drl"));
+        kfs.write(ResourceFactory.newClassPathResource("rules/forward/bed-sheet-rules.drl"));
 
         // Šabloni za haljine
         compileSpreadsheet(kfs, "rules/forward/dress/dress_base_length.drt", "rules/forward/dress/dress_base_length.xls");
